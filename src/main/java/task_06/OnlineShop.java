1a797fdedd220bc117451a7a436821ca9b616e16
klysov <thereis9000@gmail.com>
2017-07-24 15:28:24 +0300

    add task 06 online shop subtask

--- src/main/java/task_06/OnlineShop.java
+++ src/main/java/task_06/OnlineShop.java
@@ -1,8 +1,35 @@
 package task_06;
 
-public class OnlineShop implements Runnable {
-    @Override
-    public void run() {
+public class OnlineShop {
+    private static int sausageStock = 100;
 
+    public static void main(String[] args) {
+        buyMultipleSuasages(5);
+    }
+
+    /**
+     * Creates specified number of threads to make deals
+     *
+     * @param numberOfDeals - number of threads to initiate
+     */
+    private static void buyMultipleSuasages(int numberOfDeals) {
+        for (int i = 0; i < numberOfDeals; i++) {
+            new Thread(() -> buySausages(25)).start();
+        }
+    }
+
+    /**
+     * Removes specified number of sausages from stock
+     *
+     * @param numberOfSausagesToBuy - sausages to buy
+     */
+    private synchronized static void buySausages(int numberOfSausagesToBuy) {
+        if (numberOfSausagesToBuy <= sausageStock) {
+            sausageStock -= numberOfSausagesToBuy;
+            System.err.println("Sausages left: " + sausageStock);
+        } else {
+            System.err.println("There is no sausages left");
+            System.err.println("Remember, gluttony is a sin");
+        }
     }
 }
