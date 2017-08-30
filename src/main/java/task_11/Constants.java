fc85f09838b4023aaca3cc4628d8fd6d5cf34078
klysov <thereis9000@gmail.com>
2017-08-30 08:02:25 +0300

    cosmetic changes in task 11

--- src/main/java/task_11/Constants.java
+++ src/main/java/task_11/Constants.java
@@ -3,12 +3,11 @@ package task_11;
 import java.io.File;
 
 public class Constants {
-
     static final String DIR_STRING = System.getProperty("user.dir") + "\\src\\main\\java\\task_11\\DATA";
-    static final String OUTPUT_NAME = "output.csv";
-    static final String HEADER = "user,url,time";
-    static final String EXTENSION = ".csv";
+    static final String OUTPUT_NAME = "OUTPUT.csv";
     static final String DELIMITER = ",";
+    static final String HEADER = "user" + Constants.DELIMITER + "url" + Constants.DELIMITER + "time";
+    static final String EXTENSION = ".csv";
     static final File DIRECTORY = new File(System.getProperty("user.dir") + "\\src\\main\\java\\task_11\\DATA");
     static final int THREAD_AMOUNT = 10;
 }
